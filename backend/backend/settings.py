3003386972e33e72af0131a2cc7a8cf9df0cb3be
Arul Deepika <aruldeepika6@gmail.com>
2026-09-16 17:37:19 +0530

    Allow Render host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-+&p^er#xk698pjb-8dqo#5mvey!dmz3nm##mdurq1@@&lm5!m+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["student-management-backend-92a9.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
